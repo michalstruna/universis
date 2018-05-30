c10c2d95f6a36958927a4c6a5d48cd74cc29839f
Michal Struna <michal.l.struna@gmail.com>
2018-05-30 22:14:13 +0200

    Add body model.

--- src/Database/Schemas/BodySchema.ts
+++ src/Database/Schemas/BodySchema.ts
@@ -4,23 +4,34 @@ import { Schema } from 'mongoose'
  * DB schema for short body.
  */
 const BodySchema = new Schema({
-
-    _id: {
-        type: String,
-        required: true
-    },
-
-    name: {
-        type: String,
-        required: true
-    },
-
+    name: { type: String, required: true, minlength: 1 },
     diameter: {
-        equatorial: {
-            type: Number,
-            required: true
-        }
-    }
+        equatorial: { type: Number, required: true, min: 0 },
+        polar: { type: Number, required: true, min: 0 }
+    },
+    orbit: {
+        apocenter: { type: Number, required: true, min: 0 },
+        pericenter: { type: Number, required: true, min: 0 },
+        eccentricity: { type: Number, required: true, min: 0, max: 2 },
+        inclination: { type: Number, required: true, min: -360, max: 360 },
+        startAngle: { type: Number, required: true, min: 0, max: 360 },
+        period: { type: Number, required: true, min: 0 }
+    },
+    period: { type: Number, required: true, min: 0 },
+    rings: {
+        type: [{
+            diameter: {
+                min: { type: Number, required: true, min: 0 },
+                max: { type: Number, required: true, min: 1 }
+            },
+            texture: { type: Number, required: true, minlength: 1 }
+
+        }],
+        required: true
+    },
+    texture: { type: String, required: true, minlength: 1 },
+    tilt: { type: Number, required: true, min: -360, max: 360 },
+    type: { type: Number, required: true }
 })
 
 export default BodySchema
--- src/Models/UniverseModel.ts
+++ src/Models/UniverseModel.ts
@@ -22,26 +22,26 @@ class UniverseModel extends Model implements IUniverseModel {
 
     public getBodyById(bodyId: string, token: string): Promise<IBody> {
         return new Promise((resolve, reject) => {
-            this.db.getModel(this.dbModels.BODY).find({
-                _id: bodyId
-            }, (error, body) => {
-                error ? reject(INTERNAL_SERVER_ERROR) : resolve(body)
+            this.db.getModel(this.dbModels.BODY).findById(bodyId, (error, body) => {
+                error ? reject(INTERNAL_SERVER_ERROR) : resolve(body[0])
             })
         })
     }
 
     public removeBodyById(bodyId: string, token: string): Promise<number> {
         return new Promise((resolve, reject) => {
-            this.db.getModel(this.dbModels.BODY).remove({
-                _id: bodyId
-            }, (error, count) => {
+            this.db.getModel(this.dbModels.BODY).findByIdAndRemove(bodyId,(error, count) => {
                 error || !count ? reject(INTERNAL_SERVER_ERROR) : resolve(count)
             })
         })
     }
 
     public updateBody(body: IBody, token: string): Promise<void> {
-        throw new Error()
+        return new Promise((resolve, reject) => {
+            this.db.getModel(this.dbModels.BODY).findByIdAndUpdate(body._id, body, error => {
+                error ? reject(INTERNAL_SERVER_ERROR) : resolve()
+            })
+        })
     }
 
 }
