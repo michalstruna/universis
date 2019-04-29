4096556e70aa6d9467680ddc4bdcc379ef1f0b2b
Michal <michal.l.struna@gmail.com>
2019-04-29 20:46:14 +0200

    Add custom user query.

--- src/Models/UserModel.ts
+++ src/Models/UserModel.ts
@@ -2,5 +2,11 @@ import { DatabaseModel } from '../Constants'
 import ItemModel from './ItemModel'
 
 export default new ItemModel<Universis.User, Universis.User.Simple, Universis.User.New>({
-    dbModel: DatabaseModel.USER
+    dbModel: DatabaseModel.USER,
+    get: {
+        custom: filter => ([
+            { $match: filter },
+            { $lookup: { from: 'postvotes', localField: '_id', foreignField: 'userId', as: 'inVotes' } }
+        ])
+    }
 })
