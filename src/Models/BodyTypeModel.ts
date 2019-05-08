16b3589cae52b38f6836d46d75f78598c4ea0852
Michal <michal.l.struna@gmail.com>
2019-05-08 18:52:22 +0200

    Add before delete body type check.

--- src/Models/BodyTypeModel.ts
+++ src/Models/BodyTypeModel.ts
@@ -1,5 +1,6 @@
-import { DatabaseModel, SubjectType } from '../Constants'
+import { DatabaseModel, Errors, SubjectType } from '../Constants'
 import EntityModel from './ItemModel'
+import BodyModel from './BodyModel'
 
 export default new EntityModel<Universis.Universe.Body.Type, Universis.Universe.Body.Type, Universis.Universe.Body.Type.New>({
     dbModel: DatabaseModel.BODY_TYPE,
@@ -16,7 +17,14 @@ export default new EntityModel<Universis.Universe.Body.Type, Universis.Universe.
     },
     remove: {
         notification: true,
-        approval: true
+        approval: true,
+        onBefore: async (filter) => {
+            const bodiesCount = await BodyModel.count({ typeId: filter._id })
+
+            if (bodiesCount > 0) {
+                return Promise.reject(Errors.INTEGRITY_BREAK)
+            }
+        }
     },
     update: {
         notification: true,
